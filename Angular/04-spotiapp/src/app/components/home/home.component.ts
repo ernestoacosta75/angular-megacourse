e1beb8e9f62984580511f8392c6596de87a58afd
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2019-06-07 15:49:50 +0200

    Handling error of Observable

--- Angular/04-spotiapp/src/app/components/home/home.component.ts
+++ Angular/04-spotiapp/src/app/components/home/home.component.ts
@@ -9,15 +9,26 @@ import { SpotifyService } from "@app/services/spotify.service";
 export class HomeComponent implements OnInit {
   newSongs: any[] = [];
   loading: boolean;
+  error: boolean;
+  errorMessage: string;
 
   constructor(private _spotifyService: SpotifyService) {
     this.loading = true;
+    this.error = false;
 
-    this._spotifyService.getNewReleasesList().subscribe((data: any) => {
-      console.log(data);
-      this.newSongs = data;
-      this.loading = false;
-    });
+    this._spotifyService.getNewReleasesList().subscribe(
+      (data: any) => {
+        console.log(data);
+        this.newSongs = data;
+        this.loading = false;
+      },
+      errorServizio => {
+        this.loading = false;
+        this.error = true;
+        this.errorMessage = errorServizio.error.error.message;
+        console.log(errorServizio);
+      }
+    );
   }
 
   ngOnInit() {}

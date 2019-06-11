814ed5e992b26eb4a9e0f4e9097843136da8fdaa
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2019-06-11 11:04:34 +0200

    Navigation between pages. Created add list page.

--- Angular/05-todolist/src/app/pages/tab1/tab1.page.ts
+++ Angular/05-todolist/src/app/pages/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DesiresService } from 'src/app/services/desires.service';
 import { List } from 'src/app/models/list.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
@@ -9,7 +10,14 @@ import { List } from 'src/app/models/list.model';
 })
 export class Tab1Page {
 
-  constructor( public desiresService: DesiresService  ) {
+  constructor( public desiresService: DesiresService, 
+                private _router: Router  ) {
   }
 
+  /**
+   * To navigate to the add page.
+   */
+  addList() {
+    this._router.navigateByUrl(this._router.url + "/add");
+  }
 }

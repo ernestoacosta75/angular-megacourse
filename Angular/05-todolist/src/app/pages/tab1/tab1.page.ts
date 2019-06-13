c249d8982e7f7d77dcb3489083920a5d8d5d5cb2
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2019-06-13 08:43:08 +0200

    AlertController-Added a new list to the service.

--- Angular/05-todolist/src/app/pages/tab1/tab1.page.ts
+++ Angular/05-todolist/src/app/pages/tab1/tab1.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { DesiresService } from 'src/app/services/desires.service';
 import { List } from 'src/app/models/list.model';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -11,13 +12,48 @@ import { Router } from '@angular/router';
 export class Tab1Page {
 
   constructor( public desiresService: DesiresService, 
-                private _router: Router  ) {
+                private _router: Router,
+                private _alertController: AlertController ) {
   }
 
   /**
    * To navigate to the add page.
    */
-  addList() {
-    this._router.navigateByUrl(this._router.url + "/add");
+  async addList() {
+    //this._router.navigateByUrl(this._router.url + "/add");
+    const alert = await this._alertController.create({
+      header: 'New list',
+      inputs: [
+        {
+          name: "title",
+          type: "text",
+          placeholder: "List name"
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          handler: () => {
+            console.log("Cancel");
+          }
+        },
+        {
+          text: "Create",
+          handler: ( data ) => {
+            console.log(data);
+            if (data.title.length == 0) {
+              return;
+            }
+            else {
+              //Creating the list
+              this.desiresService.createList(data.title );
+            }
+          }
+        }          
+      ]
+    });
+
+    alert.present();
   }
 }
--- Angular/05-todolist/src/app/services/desires.service.ts
+++ Angular/05-todolist/src/app/services/desires.service.ts
@@ -14,4 +14,13 @@ export class DesiresService {
     this.lists.push(list1, list2);
     console.log(this.lists);
   }
+
+  /**
+   * To create a new list.
+   * @param title List title
+   */
+  createList( title: string) {
+    const newList = new List(title);
+    this.lists.push(newList);
+  }
 }
